bf73795200e528a2ffaf66783d911c2924711b0c
GreatRacc <EvilGrea@yandex.ru>
2024-09-07 02:55:22 +0400

    Задания 7.8 (1,2,3).

--- src/HomeWork_7_8_3.java
+++ src/HomeWork_7_8_3.java
@@ -1,8 +1,4 @@
 public class HomeWork_7_8_3 { // Задание 3.
     public static void main(String[] args) {
-        long l = 9_999_999_999L; // Данное число явно больше чем 2_147_483_647
-        int i = (int) l;
-        System.out.println(l + " - Это переменная типа long.");
-        System.out.println(i + " - Это переменная типа int с которой произошло переполнение.");
     }
 }
