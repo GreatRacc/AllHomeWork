da3772af3b642d72dd6e056b8635fae5d30795fe
GreatRacc <EvilGrea@yandex.ru>
2024-10-05 21:21:29 +0400

    05.10.2024 - выполнено.

--- src/HomeWork_5/mainClass.java
+++ src/HomeWork_5/mainClass.java
@@ -16,9 +16,15 @@ public class mainClass {
         System.out.println(Methods.printFullName(input(), input()));
         System.out.println("Самая длинная и коротка строка это: ");
         System.out.println(Methods.cornerValuesArray("раз", "два", "три", "четыре", "пять"));
+        System.out.println(Methods.cornerValuesArray(new String[]{}));
         System.out.println("Самое малое и большое число это: ");
         System.out.println(Methods.cornerValuesArray(10, -45, 76, 34, 12, -12));
+        System.out.println(Methods.cornerValuesArray(new int[]{}));
         System.out.println("Самый малый и большой символ это: ");
         System.out.println(Methods.cornerValuesArray('H', 'a', 'g', 'D', 'v', 'c'));
+        System.out.println(Methods.cornerValuesArray(new char[]{}));
+        System.out.println("факториал от вашего числа равен: ");
+        System.out.println(Methods.factorialNum(0));
+        System.out.println(Methods.factorialNum(10));
     }
 }
