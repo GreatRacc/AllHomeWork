ce1810ad54e4d433d93296599b79ef88ca9eaef5
GreatRacc <EvilGrea@yandex.ru>
2024-12-15 15:58:57 +0400

    07.12.2024 - выполнено.

---
+++ src/main/java/HomeWork_18/Book.java
@@ -0,0 +1,40 @@
+package HomeWork_18;
+
+public class Book {
+    private final int id;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(int id, String title, String author, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "ID = " + id +
+                ", Название книги = '" + title + '\'' +
+                ", Автор = '" + author + '\'' +
+                ", Доступна? = " + isAvailable +
+                '.';
+    }
+}
